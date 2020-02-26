71ec446a89bded3628f984a9468137d43942baa7
Jose De La Rosa <josedlr93@aol.com>
2020-02-26 14:59:38 -0500

    Create branch for webdriver setup
     - Add conditionals for different platforms
    
     - add gitignore

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,11 +1,28 @@
 import time
+import platform
 from selenium import webdriver
 
-driver = webdriver.Chrome('/path/to/chromedriver')  # Optional argument, if not specified will search path.
-driver.get('http://www.google.com/')
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('q')
-search_box.send_keys('ChromeDriver')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
+# Use platform.system() to check platform string for conditionals below
+# print(platform.system())
+
+driver = None
+
+# Check Chrome version to download appropriate binaries
+# Add binaries to directory (drivers) and specify executable path in Chrome()
+if platform.system() == "Windows":
+    driver = webdriver.Chrome(executable_path='drivers/chromedriver_win32/chromedriver.exe')  # optional argument, if not specified will search path.
+elif platform.system() == 'Linux':
+    driver = webdriver.Chrome(executable_path='drivers/chromedriver_win32/chromedriver.exe')
+elif platform.system() == 'Darwin':
+    driver = webdriver.Chrome(executable_path='drivers/chromedriver_win32/chromedriver.exe')
+else:
+    driver = webdriver.Chrome()
+
+if driver:
+    driver.get('http://www.google.com/')
+    time.sleep(5) # let the user actually see something!
+    search_box = driver.find_element_by_name('q')
+    search_box.send_keys('chromedriver')
+    search_box.submit()
+    time.sleep(5) # let the user actually see something!
+    driver.quit()
