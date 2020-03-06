fb1fd2bf18d93d896e75e5279e07856417daeb30
Jose De La Rosa <josedlr93@aol.com>
2020-03-06 13:53:25 -0500

    refactor
    - move driver initialize to webhelper

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,46 +1,27 @@
 import time
-import platform
-from selenium import webdriver
 from helpers import webHelper
 
-# Use platform.system() to check platform string for conditionals below
-# print(platform.system())
-
-driver = None
-
-# Check Chrome version to download appropriate binaries
-# Add binaries to directory (drivers) and specify executable path in Chrome()
-if platform.system() == "Windows":
-    driver = webdriver.Chrome(executable_path='drivers/chromedriver_win32/chromedriver.exe')  # optional argument, if not specified will search path.
-elif platform.system() == 'Linux':
-    driver = webdriver.Chrome(executable_path='drivers/chromedriver.exe')
-elif platform.system() == 'Darwin':
-    driver = webdriver.Chrome(executable_path='drivers/chromedriver')
-else:
-    driver = webdriver.Chrome()
-
+driver = webHelper.initiate_driver()
 # driver = None
-address = 'www.towson.edu'
+address = "www.w3schools.com/jsref/tryit.asp?filename=tryjsref_form_reset"  #'www.towson.edu'
 url = 'https://' + address
 if driver:
     try:
         driver.get(url)
-
-        ## Sample test
-        # time.sleep(5)  # let the user actually see something!
-        # search_box = driver.find_element_by_name('q')
-        # search_box.send_keys('chromedriver')
-        # search_box.submit()
-        # time.sleep(5) # let the user actually see something!
+        time.sleep(10)
 
         ## Save source
         # html = driver.page_source
         # print(webHelper.save_source(address, html))
 
-        ## Get Element fonts
+        ## check Element fonts
         # webHelper.get_element_fonts(driver, "*")
 
-        ## Get Element inner html
-        webHelper.get_inner_html(driver)
+        ## check Element inner html (incomplete)
+        # webHelper.get_inner_html(driver)
+
+        ## Check consistent use of color
+        # webHelper.get_element_colors(driver)
+
     finally:
         driver.quit()
--- helpers/webHelper.py
+++ helpers/webHelper.py
@@ -1,6 +1,37 @@
 import os
 import time
+import platform
+from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException
+
+
+def initiate_driver():
+    # Use platform.system() to check platform string for conditionals below
+    # print(platform.system())
+
+    driver = None
+
+    # Check Chrome version to download appropriate binaries
+    # Add binaries to directory (drivers) and specify executable path in Chrome()
+    if platform.system() == "Windows":
+        driver = webdriver.Chrome(
+            executable_path='drivers/chromedriver_win32/chromedriver.exe')  # optional argument, if not specified will search path.
+    elif platform.system() == 'Linux':
+        driver = webdriver.Chrome(executable_path='drivers/chromedriver.exe')
+    elif platform.system() == 'Darwin':
+        driver = webdriver.Chrome(executable_path='drivers/chromedriver')
+    else:
+        driver = webdriver.Chrome()
+
+    return driver
+
+
+## Sample test
+# time.sleep(5)  # let the user actually see something!
+# search_box = driver.find_element_by_name('q')
+# search_box.send_keys('chromedriver')
+# search_box.submit()
+# time.sleep(5) # let the user actually see something!
 
 
 def save_source(address, source):
@@ -19,7 +50,6 @@ def save_source(address, source):
 # Stale Element Exception is raised if wait is not provided
 # Need to find a better method than sleep()
 def get_element_fonts(driver, selector):
-    time.sleep(5)
     elements = driver.find_elements_by_css_selector(selector)
     fonts = []
     for i, e in enumerate(elements):
@@ -34,8 +64,7 @@ def get_element_fonts(driver, selector):
     print("Number of fonts (including fallbacks): ", len(fonts))
 
 
-def get_inner_html(driver):
-    time.sleep(5)
+def get_inner_html(driver):  # incomplete
     elements = driver.find_elements_by_css_selector("*")
     text = ""
     for i, e in enumerate(elements):
@@ -47,3 +76,15 @@ def get_inner_html(driver):
             print(e)
     print(text)
 
+
+def get_element_colors(driver):
+    elements = driver.find_elements_by_css_selector("*")
+    colors = []
+    for i, e in enumerate(elements):
+        try:
+            if e.value_of_css_property('display') != "none":
+                inner = driver.execute_script("return arguments[0].textContent", e)
+                text = text + " " + inner
+        except StaleElementReferenceException as e:
+            print(e)
+    print(colors)
